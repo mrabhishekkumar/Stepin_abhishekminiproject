db446e98f4de11732fe594ae1f33d2ae12be2fbb
mrabhishekkumar <rishuzb945@gmail.com>
2021-09-07 18:40:17 +0530

    hea

---
+++ 3.Implementation/inc/header.h
@@ -0,0 +1,10 @@
+#ifndef __HEADER_H
+#define __HEADER_H
+  void team();
+    void player();
+    void schedule();
+    void stats_season();
+    void stats_player();
+    void umpires();
+    void owners();
+    #endif
