43be87ac59e3a1ad09b9bb1122facbc7be5ef345
mrabhishekkumar <rishuzb945@gmail.com>
2021-09-07 18:37:17 +0530

    cod

--- 3.Implementation/main.c
+++ 3.Implementation/main.c
@@ -1,13 +1,6 @@
 #include<stdio.h>
 #define __HEADER_H
-    void team();
-    void player();
-    void schedule();
-    void stats_season();
-    void stats_player();
-    void umpires();
-    void owners();
-
+    
 
 int main(){
     char ch;
